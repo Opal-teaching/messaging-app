a7b2d95220e8e8fd2106390807d06cfc7428c703
Eason <tong.yang@mail.mcgill.ca>
2018-05-11 09:57:30 -0400

    with camment

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,75 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "messaging-app.controller:ConversationsController",
+      "shortName": "ConversationsController",
+      "type": "controller",
+      "moduleName": "messaging-app",
+      "shortDescription": "Controller for ./views/messages/conversations.html",
+      "keywords": "$scope $timeout animation api array controller conversation conversations conversationscontroller determine emptyconversations event flag gotoconversation html initcontroller initializeevents initializes lift matches message messaging-app messengerservice method names navigator new-conversation newconversation parameter property pushes refresh scope search searchconversationstring selected service stack string variables"
+    },
+    {
+      "section": "api",
+      "id": "messaging-app.controller:IndividualConversationController",
+      "shortName": "IndividualConversationController",
+      "type": "controller",
+      "moduleName": "messaging-app",
+      "shortDescription": "Manages  ./views/messages/individual-conversation.html",
+      "keywords": "$timeout add alert api array call calls case contents controller conversation conversations current delete deleteconversation display displays empty fails flag html individualconversationcontroller initcontroller initializes manages message messagecontent messages messaging-app messengerservice method nomessages pops property sendmessage service updates vm"
+    },
+    {
+      "section": "api",
+      "id": "messaging-app.controller:NewConversationController",
+      "shortName": "NewConversationController",
+      "type": "controller",
+      "moduleName": "messaging-app",
+      "shortDescription": "Manages  ./views/messages/new-conversation.html",
+      "keywords": "$scope $timeout add addconversation api button calls checkfields checks controller conversation createconversation fields html imageurl manages messaging-app messengerservice method newconversationcontroller ngdisabled person property service submit url user"
+    },
+    {
+      "section": "api",
+      "id": "messaging-app.service:MessengerService",
+      "shortName": "MessengerService",
+      "type": "service",
+      "moduleName": "messaging-app",
+      "shortDescription": "Model for the chat application, manipulates the conversations array",
+      "keywords": "addconversation adds api app application array based chat conversation conversationid conversations deleteconversation deletes failure flag getconversationbyid getconversations getter image imageurl manipulates matching message messagecontent messaging messaging-app messengerservice method model null person property returns searches sendmessage service success takes url user"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js"
+    ],
+    "styles": [],
+    "title": "API Documentation",
+    "html5Mode": true,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true
+    }
+  },
+  "html5Mode": true,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "js/angular.min.js",
+    "js/angular-animate.min.js",
+    "js/marked.js"
+  ]
+};
